acadce8b0788503d35b824d6abcdb21d07e55eb2
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-08-08 16:45:50 +0700

    init project

---
+++ src/main/java/net/cec/HomeServlet.java
@@ -0,0 +1,26 @@
+package net.cec;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+    name = "HomeServlet",
+    urlPatterns = {"/"}
+)
+public class HomeServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+
+    response.setContentType("text/plain");
+    response.setCharacterEncoding("UTF-8");
+
+    response.getWriter().print("Hello App Engine!\r\n");
+
+  }
+}
